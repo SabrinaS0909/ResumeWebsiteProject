2bc88d91d9939bf7fc5a6d5096b35274716bdd8e
Sabrina Spruck <113223660+SabrinaS0909@users.noreply.github.com>
2023-02-24 03:15:06 -0500

    tried a lot of things
    made going from desktop to phone more responsive;
    tried to make links fade when you're on their respective page;
    tried to put innerHTML onto the Coming Soon page

--- smallerBrains.js
+++ smallerBrains.js
@@ -1,5 +1,63 @@
 "use strict";
 
+//to make the size work on phone and desktop
+
+/*window.onload = function() {
+let w = document.documentElement.clientWidth || window.innerWidth;
+let h = document.documentElement.clientWidth || window.innerHeight;
+let cwc = document.querySelector('.certWindowContainer');
+let x = document.querySelector('.xButton');
+let wCert = document.querySelector('.webDevelopmentCert');
+let sqlCert = document.querySelector('.sqlCert');
+let pCert = document.querySelector('.pythonCert');
+let d = document.querySelector('.dot');
+let la = document.querySelector('.leftArrow');
+let ra = document.querySelector('.rightArrow');
+
+if (w <= 1080 && h <= 1920 ) {
+document.getElementById('projectOneLinkTop').style.backgroundColor = 'red';
+document.querySelector('.buttonToOpenCerts').style.fontSize = '24px';
+document.querySelector('.bottomCerts').style.fontSize = '16px';
+document.querySelector('.footer').style.fontSize = '9.5px';
+cwc.style.maxWidth = '900px';
+cwc.style.maxHeight = '1500px';
+cwc.style.width = '100%';
+cwc.style.height = '100%';
+x.style.fontSize = '40px';
+x.style.height = '75px';
+x.style.width = '75px';
+x.style.margin = '0px';
+x.style.padding = '0px';
+x.style.alignItems = 'center';
+wCert.style.maxHeight = ('900px');
+wCert.style.maxWidth = ('650px');
+wCert.style.height = ('1500px');
+wCert.style.width = ('750px');
+sqlCert.style.maxHeight = ('900px');
+sqlCert.style.maxWidth = ('650px');
+sqlCert.style.height = ('1500px');
+sqlCert.style.width = ('750px');
+pCert.style.maxHeight = ('900px');
+pCert.style.maxWidth = ('650px');
+pCert.style.height = ('1500px');
+pCert.style.width = ('750px');
+d.style.width = ('25px');
+d.style.height = ('25px');
+d.style.borderRadius = ('50px');
+d.style.padding = ('20px');
+d.style.margin = ('25px');
+d.style.position = ('relative');
+d.style.right = ('50%');
+la.style.width = ('125px');
+la.style.height = ('80px');
+ra.style.width = ('125px');
+ra.style.height = ('80px');
+}
+else {
+console.log('Desktop');
+};
+};
+
 //if you're on a page, it should be dulled with no ability to click it - top and bottom
 
 
@@ -81,5 +139,14 @@ projectTwoGithubBottom.addEventListener('click', function () {
 projectThreeGithubBottom.addEventListener('click', function () {
 	dimProjectThree();
 });
-
+*/
 //inserts the correct project desc when the right button is pressed
+window.onload =function () {
+	let title = document.querySelector('.title');
+function projectTwo() {
+	document.body.h2.innerHTML = 'Story Writing App';
+	title.style.backgroundColor = 'red';
+};
+function projectThree() {
+	document.body.h2.innerHTML = 'Spider Identification App';
+};};
